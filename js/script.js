6ded7e0618e05c486c8af6dc71fa46272f58a229
Anna <averbah-ann@mail.ru>
2020-11-12 14:23:55 +0400

    Сломала блок Reasons, не дописала слайде

--- js/script.js
+++ js/script.js
@@ -1,20 +1,38 @@
-$(document).ready(function(){
-  $('.reasons__slider').slick({
-    dots: true,
-    arrows: false
-  });
+window.addEventListener('DOMContentLoaded', function() {
+	'use strict'
 
-  let menu = document.querySelector('.header__menu'),
-    burger = document.querySelector('.menu__burger'),
-    close = document.querySelector('.menu__closed');
+	// <Меню бургер> ========================================================================== 
+	let menu = document.querySelector('.header__menu'),
+	burger = document.querySelector('.menu__burger'),
+	close = document.querySelector('.menu__closed');
 
-burger.addEventListener('click', () => {
-  menu.classList.add('active');
-});
+	burger.addEventListener('click', () => {
+	menu.classList.add('active');
+	});
 
-close.addEventListener('click', () => {
-  menu.classList.remove('active');
-});
+	close.addEventListener('click', () => {
+	menu.classList.remove('active');
+	});
+	// </Меню бургер> =========================================================================
+
+
+	// <Слайдер> ========================================================================== 
+	let sliderIndex = 1,
+		slides = document.querySelectorAll('.reasons__slider-item'),
+		dotsWrap = document.querySelector('.slider-dots'),
+		dots = document.querySelectorAll('.dot');
+
+		showSlides(sliderIndex);
+
+		function showSlides(n) {
+			slides.forEach((item) => item.style.display = 'none');
+			dots.forEach((item) => item.classList.remove('dot-active'));
+
+			slides[slideIndex - 1].style.display = 'block';
+			dots[sliderIndex - 1].classList.add('dot-active');
+		}
+
+	// </Слайдер> ========================================================================== 
 
 });
 
